3804dcf7dfe625369729a84834b0378d93256859
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-10-03 21:07:28 +0200

    test: custom when function provided through module

--- src/lib/src/sin.directive.spec.ts
+++ src/lib/src/sin.directive.spec.ts
@@ -4,32 +4,32 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing'
 import {SinModule} from './sin.module'
 import {By} from '@angular/platform-browser'
 
+@Component({
+  template: `
+    <form [formGroup]="form">
+      <label>
+        <span>Username</span>
+        <input type="text" formControlName="username">
+      </label>
+      <div *ngxSin="'required'; control: form.get('username')">Username is required</div>
+    </form>
+  `,
+})
+class TestComponent {
+  form = this.fb.group({
+    username: ['', [Validators.required]],
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+}
+
+let fixture: ComponentFixture<TestComponent>
+let testHost: TestComponent
+
 describe(`Sin Directive`, () => {
 
   describe(`with default when function`, () => {
-
-    @Component({
-      template: `
-        <form [formGroup]="form">
-          <label>
-            <span>Username</span>
-            <input type="text" formControlName="username">
-          </label>
-          <div *ngxSin="'required'; control: form.get('username')">Username is required</div>
-        </form>
-      `,
-    })
-    class TestComponent {
-      form = this.fb.group({
-        username: ['', [Validators.required]],
-      })
-
-      constructor(private fb: FormBuilder) {
-      }
-    }
-
-    let fixture: ComponentFixture<TestComponent>
-    let testHost: TestComponent
 
     beforeEach(async(() => {
       TestBed.configureTestingModule({
@@ -78,6 +78,40 @@ describe(`Sin Directive`, () => {
       testHost.form.setValue({username: ''})
       fixture.detectChanges()
       expect(fixture.debugElement.queryAll(By.css('div')).length).toBe(1, 'appeared again')
+    })
+
+  })
+
+
+  describe(`with custom when function provided through module`, () => {
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [TestComponent],
+        imports: [
+          ReactiveFormsModule,
+          SinModule.forRoot({when: ({dirty}) => dirty}),
+        ],
+      }).compileComponents()
+    }))
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestComponent)
+      testHost = fixture.componentInstance
+      fixture.detectChanges()
+    })
+
+    it(`should not display any errors initially`, () => {
+      expect(fixture.debugElement.queryAll(By.css('div')).length).toBe(0)
+    })
+
+    it(`should display error only after becoming dirty`, () => {
+      expect(fixture.debugElement.queryAll(By.css('div')).length).toBe(0, `no error when pristine`)
+
+      testHost.form.get('username').markAsDirty()
+      fixture.detectChanges()
+      expect(fixture.debugElement.queryAll(By.css('div')).length)
+        .toBe(1, `visible error when dirty`)
     })
 
   })
