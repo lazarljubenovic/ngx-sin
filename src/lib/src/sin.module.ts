75de733cd12fbb5b69c6357679372ff0c74f4c66
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-10-04 22:56:33 +0200

    fix: use a factory for sake of aot

--- src/lib/src/sin.module.ts
+++ src/lib/src/sin.module.ts
@@ -4,7 +4,15 @@ import {ReactiveFormsModule} from '@angular/forms'
 import {SinDirective} from './sin.directive'
 import {SIN_CONFIG} from './sin-config'
 import {SinsDirective} from './sins.directive'
-import {SinModuleConfig} from './interfaces'
+import {SinModuleConfig, WhenObject} from './interfaces'
+
+export function when({dirty, touched}: Partial<WhenObject>) {
+  return dirty && touched;
+}
+
+export const defaultConfig = {
+  when,
+}
 
 @NgModule({
   imports: [
@@ -24,18 +32,10 @@ export class SinModule {
   public static forRoot(config?: Partial<SinModuleConfig>): ModuleWithProviders {
     return {
       ngModule: SinModule,
-      providers: [
-        {
-          provide: SIN_CONFIG,
-          useValue: {
-            ...{
-              when: function DEFAULT_WHEN_FN({dirty, touched}: any) {
-                return dirty && touched
-              },
-            }, ...(config || {})
-          },
-        },
-      ],
+      providers: [{
+        provide: SIN_CONFIG,
+        useValue: defaultConfig,
+      }],
     }
   }
 }
